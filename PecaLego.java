2d3b35da2354c6a4608c1395e4bdb62e39568236
aluno <@>
2024-09-24 17:10:45 -0300

    Aula 24/09

---
+++ PecaLego.java
@@ -0,0 +1,7 @@
+public class PecaLego {
+    public static void main(String[] args) {
+        PecaLego minhaPeca = new PecaLego();
+        System.out.println("Uma peça Lego foi criada!");
+    }
+ }
+ 
