3c3c79d923c8146623e2fa1c9fc2eac07e6571ca
aluno <you@example.com>
2024-10-01 15:59:31 -0300

    Aula 6 01/10/2024

--- PecaLego.java
+++ PecaLego.java
@@ -1,7 +1,39 @@
 public class PecaLego {
-    public static void main(String[] args) {
-        PecaLego minhaPeca = new PecaLego();
-        System.out.println("Uma peça Lego foi criada!");
+    // Atributos (características da peça)
+    String cor;
+    String formato;
+    int numeroDePinos;
+
+    // Métodos (ações que a peça pode realizar)
+    public void mostrarDetalhes() {
+        System.out.println("Peça Lego " + cor + " com " + numeroDePinos + " pinos, formato " + formato + ".");
     }
- }
- 
+
+    public void girar() {
+        System.out.println("Peça Lego " + cor + " girou 90 graus!");
+    }
+
+    public static void main(String[] args) {
+        // Criando objetos (peças Lego reais)
+        PecaLego pecaVermelha = new PecaLego();
+        PecaLego pecaAzul = new PecaLego();
+
+        // Definindo características da peça vermelha
+        pecaVermelha.cor = "Vermelha";
+        pecaVermelha.formato = "Retangular";
+        pecaVermelha.numeroDePinos = 4;
+
+        // Definindo características da peça azul
+        pecaAzul.cor = "Azul";
+        pecaAzul.formato = "Quadrado";
+        pecaAzul.numeroDePinos = 2;
+
+        // Fazendo as peças realizarem ações
+        pecaVermelha.mostrarDetalhes();
+        pecaAzul.mostrarDetalhes();
+
+        pecaVermelha.girar();
+        pecaAzul.girar();
+    }
+}
+
