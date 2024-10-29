c4b184a27449cefaa64985e4808b05b8099bf042
Your Name <you@example.com>
2024-10-29 16:03:10 -0300

    .

--- G2StringSubstring.java
+++ G2StringSubstring.java
@@ -2,7 +2,7 @@
 public class G2StringSubstring {
     public static void main(String[] args) {
         String str1 = "Curso C"; //string literal atribuida a str1
-        String str2 = str1.substring(0, 5);// 
+        String str2 = str1.substring(1, 5);// 
         System.out.println("str2 = " + str2);
     }
 }
